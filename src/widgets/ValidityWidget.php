11f94d931dac131791f3a5c0860a291fc8eea1dc
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-12-07 17:00:01 +0200

    Added ValidityWidget

---
+++ src/widgets/ValidityWidget.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace hipanel\modules\document\widgets;
+
+use hipanel\modules\document\models\Document;
+use Yii;
+use yii\base\Widget;
+
+class ValidityWidget extends Widget
+{
+    /**
+     * @var Document
+     */
+    public $model;
+
+    /**
+     * @var string
+     */
+    public $start_attribute = 'validity_start';
+
+    /**
+     * @var string
+     */
+    public $end_attribute = 'validity_end';
+
+    protected function getStart()
+    {
+        return $this->model->{$this->start_attribute};
+    }
+
+    protected function getEnd()
+    {
+        return $this->model->{$this->end_attribute};
+    }
+
+    public function run()
+    {
+        if ($this->getStart()) {
+            echo $this->renderStart();
+            echo $this->renderDelimiter();
+            echo $this->renderEnd();
+            return;
+        }
+
+        echo $this->renderEmpty();
+        return;
+    }
+
+    protected function renderStart()
+    {
+        return Yii::$app->formatter->asDate($this->getStart()); // todo: colorize
+    }
+
+    protected function renderDelimiter()
+    {
+        return ' &ndash; ';
+    }
+
+    protected function renderEnd()
+    {
+        if (!empty($this->getEnd())) {
+            return Yii::$app->formatter->asDate($this->getEnd());
+        }
+
+        return Yii::t('hipanel:document', '∞');
+    }
+
+    protected function renderEmpty()
+    {
+        return Yii::t('hipanel:document', 'Not set');
+    }
+}
