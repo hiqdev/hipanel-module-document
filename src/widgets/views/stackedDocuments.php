c665005423a2548387987f660aeec289ed638f76
tofid <andreyklochok@gmail.com>
2017-03-21 18:38:25 +0200

    Improved design

--- src/widgets/views/stackedDocuments.php
+++ src/widgets/views/stackedDocuments.php
@@ -5,93 +5,147 @@
  * @var \yii\web\View $this
  */
 
+use hipanel\helpers\StringHelper;
 use hipanel\modules\document\widgets\DocumentStatusIcons;
 use yii\helpers\Html;
 
+$this->registerCss(<<<CSS
+.stacked-documents .body {
+    margin: 2px;
+    display: flex;
+    align-items: flex-start;
+    flex-wrap: wrap;
+}
+    
+.stacked-documents .group {
+    margin-bottom: 15px;
+}
+
+.stacked-documents .group .statuses {
+    margin: 5px 5px;
+    overflow: auto;
+}
+
+.stacked-documents .group .title {
+    font-weight: bold;
+    margin: 1em 0;
+}    
+
+.stacked-documents .margin {
+    margin: 1px 0;
+}
+
+.stacked-documents .group .item {
+    position: relative;
+    margin: 0.3em 5px;
+    border: rgba(0, 0, 0, 0.05) 1px solid;
+    box-shadow: 0 0 1px 0 #a2958a;
+    padding: 4px 4px 0 4px;
+}
+
+.preview-box {
+    position: relative;
+    display: block;
+    padding: 10px;
+    background-color: #fff;
+    border-bottom: 1px solid #eaeef1;
+    box-shadow: 1px 2px 1px 0px #eaeef1;
+}
+
+.preview-box .preview-file {
+    margin-right: 15px;
+    position: relative;
+    float: left;
+}
+
+.preview-box .preview-file a {
+    width: 64px;
+    height: 64px;
+    display: inline-block;    
+    max-width: 100%;
+    vertical-align: middle;
+    text-align: center;
+}
+
+.preview-box .preview-file a:before {
+  content: "";
+  display: inline-block;
+  vertical-align: middle;
+  height: 100%;
+}
+
+.preview-box .preview-file a img {
+
+}
+
+.preview-box .preview-content {
+
+}
+
+.text-sm {
+    font-size: 13px;
+}
+
+.text-ellipsis {
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+CSS
+);
 ?>
-    <div class="stacked-documents">
-        <?php if (count($groups)) : ?>
-            <?php foreach ($groups as $date => $documents) :
+
+<div class="stacked-documents">
+    <?php if (count($groups)) : ?>
+        <?php foreach ($groups as $date => $documents) :
             /** @var \hipanel\modules\document\models\Document[] $documents */ ?>
             <div class="group">
                 <div class="title">
                     <i class="fa fa-calendar"></i> <?= Yii::$app->formatter->asDate($date) ?>
                 </div>
-                <div class="body">
-                        <?php foreach ($documents as $document) : ?>
-                            <div class="item">
-                                <div class="preview">
+                <div class="row">
+                    <?php $iteration = 1; ?>
+                    <?php foreach ($documents as $document) : ?>
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <div class="preview-box">
+                                <div class="preview-file">
                                     <?= \hipanel\widgets\FileRender::widget([
                                         'file' => $document->file,
                                         'thumbHeight' => $this->context->thumbSize,
                                         'thumbWidth' => $this->context->thumbSize,
+                                        'iconOptions' => [
+                                            'class' => 'fa-3x',
+                                        ],
                                         'lightboxLinkOptions' => [
                                             'data-lightbox' => 'files-' . $date,
                                         ],
                                     ]); ?>
                                 </div>
-                                <div class="statuses">
-                                    <?= DocumentStatusIcons::widget(['model' => $document]) ?>
-                                    <?= Html::a('<i class="fa fa-external-link"></i>',
-                                        ['@document/view', 'id' => $document->id], [
-                                            'class' => 'pull-right btn btn-default btn-xs',
-                                        ]) ?>
+                                <div class="preview-content clearfix">
+                                    <div class="statuses pull-right">
+                                        <?= DocumentStatusIcons::widget(['model' => $document]) ?>
+                                    </div>
+                                    <?php
+                                    $title = StringHelper::truncate($document->getDisplayTitle(), 20);
+                                    echo Html::a($title, ['@document/view', 'id' => $document->id]);
+                                    ?>
+                                    <div class="text-muted text-sm text-ecliellipsis"><?= Yii::$app->formatter->asDate($document->create_time) ?></div>
                                 </div>
                             </div>
-                        <?php endforeach; ?>
+                        </div>
+                        <?php
+                        if ($iteration % 2 == 0) {
+                            echo Html::tag('div', '', ['class' => 'clearfix']);
+                        }
+                        $iteration++;
+                        ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
         <?php endforeach; ?>
     <?php else: ?>
         <?= Yii::t('hipanel:document', 'No documents found') ?>
     <?php endif; ?>
-    </div>
-<?php
+</div>
 
-$this->registerCss(<<<CSS
-    .stacked-documents {
-    
-    }
-    
-    .stacked-documents .body {
-        margin: 2px;
-        display: flex;
-        align-items: flex-start;
-        flex-wrap: wrap;
-    }
-        
-    .stacked-documents .group {
-        margin-bottom: 15px;
-    }
-    
-    .stacked-documents .group .statuses {
-        margin: 5px 5px;
-        overflow: auto;
-    }
-
-    .stacked-documents .group .title {
-        font-weight: bold;
-        margin: 1em 0;
-    }    
-    
-    .stacked-documents .margin {
-        margin: 1px 0;
-    }
-    
-    .stacked-documents .group .item {
-        position: relative;
-        margin: 0.3em 5px;
-        border: rgba(0, 0, 0, 0.05) 1px solid;
-        box-shadow: 0 0 1px 0 #a2958a;
-        padding: 4px 4px 0 4px;
-    }
-    
-    .stacked-documents .group .item:hover {
-        box-shadow: 0 0 2px 0 #333;
-    }
-
-    .stacked-documents .group .item .preview {
-        text-align: center;
-    }
-CSS
-);
