80bdaf3e724914458bdc3413c8521b39413ba23a
Andrey Klochok <andreyklochok@gmail.com>
2022-06-07 17:48:41 +0300

    minor

--- tests/playwright/e2e/manager/document.spec.ts
+++ tests/playwright/e2e/manager/document.spec.ts
@@ -1,8 +1,7 @@
 import { test } from "@hipanel-core/tests/fixtures";
 import { expect } from "@playwright/test";
 
-
-test("Test the Document page is work @hipanel-module-document @manager @mail-out", async ({ managerPage }) => {
+test("Test the Document page is work @hipanel-module-document @manager @document", async ({ managerPage }) => {
   await managerPage.goto("/document/document/index");
   await expect(managerPage.locator(".content-header > h1")).toContainText("Documents");
 });
