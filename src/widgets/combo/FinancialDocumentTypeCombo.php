a7b060c9d6ac2d0345fbb1bccca6f2566f489b7f
Yurii Myronchuk <bladeroot@gmail.com>
2024-03-12 13:48:27 +0100

    HP-1869: add widget for selectiong financial doc types (#21)
    Co-authored-by: Yurii Myronchuk <bladeroot@hiqdev.com>

---
+++ src/widgets/combo/FinancialDocumentTypeCombo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace hipanel\modules\document\widgets\combo;
+
+use hipanel\helpers\ArrayHelper;
+use hiqdev\combo\StaticCombo;
+use hipanel\models\Ref;
+
+class FinancialDocumentTypeCombo extends StaticCombo
+{
+    final const INVOICE_TYPE = 'invoice';
+    final const PAYMENT_REQUEST_TYPE = 'proforma';
+
+    public $hasId = true;
+
+    public function init(): void
+    {
+        parent::init();
+        $this->data = $this->getDocumentTypes();
+    }
+
+    private function getDocumentTypes(): array
+    {
+
+        return array_filter(Ref::getList('type,document', 'hipanel', ['orderby' => 'no_asc']), function($v, $k) {
+            return strpos($k, static::INVOICE_TYPE) !== false || strpos($k, static::PAYMENT_REQUEST_TYPE) !== false;
+        }, ARRAY_FILTER_USE_BOTH);
+    }
+}
