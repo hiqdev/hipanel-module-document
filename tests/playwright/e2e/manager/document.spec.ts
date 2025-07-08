6e6c47acefb3456fe2ace0b3b3f6f3d68f9e9ecf
Yurii Myronchuk <bladeroot@hiqdev.com>
2025-07-08 09:55:03 +0000

    HP-2804: refactor document page test to use standard `page` object

--- tests/playwright/e2e/manager/document.spec.ts
+++ tests/playwright/e2e/manager/document.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@hipanel-core/fixtures";
 import { expect } from "@playwright/test";
 
-test("Test the Document page is work @hipanel-module-document @manager @document", async ({ managerPage }) => {
-  await managerPage.goto("/document/document/index");
-  await expect(managerPage.locator(".content-header > h1")).toContainText("Documents");
+test("Test the Document page is work @hipanel-module-document @manager @document", async ({ page }) => {
+  await page.goto("/document/document/index");
+  await expect(page.locator(".content-header > h1")).toContainText("Documents");
 });
